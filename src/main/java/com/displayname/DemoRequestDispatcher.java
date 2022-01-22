9ce79120626d98b122330d47cc90109887cb8898
talisker18 <hjoel87@gmx.ch>
2022-01-22 15:35:55 +0100

    .

---
+++ src/main/java/com/displayname/DemoRequestDispatcher.java
@@ -0,0 +1,13 @@
+package com.displayname;
+
+/**
+ * 
+ * see /javafullstack_2_backend1_servlet6/src/main/java/servlets/RegistrationServlet.java
+ * 
+ * demo example how to login. if login credentials are valid, use requestDispatcher to forward req and resp to WelcomeServlet
+ * 
+ * */
+
+public class DemoRequestDispatcher {
+
+}
